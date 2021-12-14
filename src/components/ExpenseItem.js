976c161f8d9a810ed414d55c5731e942b7e3110d
kaushikhande <kaushikhande302@gmail.com>
2021-12-14 16:05:53 +0530

    added props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,13 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
   return (
-    <div>
-      <div>Date</div>
-      <h2>Car Insurance</h2>
-      <div>239.3</div>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rs. {props.amount}</div>
+      </div>
     </div>
   );
 }
